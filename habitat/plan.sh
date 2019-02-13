f9458c957fd62bd29e1e95a3cb6f8f8b4335925e
Dan Flores <danf425@gmail.com>
2019-02-12 20:56:51 -0500

    use mu imageMagick package

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=habitatize
 pkg_origin=dmontanez
 pkg_scaffolding="core/scaffolding-ruby"
-pkg_version="0.1.0"
-pkg_deps=( core/imagemagick )
+pkg_version="0.2.0"
+pkg_deps=( dmontanez/imagemagick )
 
